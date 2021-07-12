70168c513fef8fc012e40c3c3c49851683ceab12
11914987 <68886038+11914987@users.noreply.github.com>
2021-07-12 18:44:27 +0530

    index.js

--- index.js
+++ index.js
@@ -1,22 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import {BrowserRouter, Switch , Route} from 'react-router-dom';
-import {createStore} from 'redux';
-import rootReducer from './reducer/index';
-import {Provider} from 'react-redux';
-import FavListComponent from './favourite.list.component';
+export const SET_RECIPE='SET_RECIPE';
 
-const store = createStore(rootReducer);
-store.subscribe(()=>{console.log('store',store.getState())});
-ReactDOM.render(
-    <Provider store={store}>
-        <BrowserRouter>
-            <Switch>
-                <Route exact path="/" component={App}/>
-                <Route exact path="/favorites" component={FavListComponent}/>
-            </Switch>
-        </BrowserRouter>
-    </Provider>, document.getElementById('root'));
+export function setRecipe(items)
+{
+    return {
+        type:SET_RECIPE,
+        items:items
+    }
+}
 
+export const FAV_RECIPE = 'FAV_RECIPE';
+
+export function setFavourite(id){
+    return {
+        type:FAV_RECIPE,
+        id:id
+    };
+}
+
+export const DEL_FAV_RECIPE = 'DEL_FAV_RECIPE';
+
+export function delFavourite(id){
+    return {
+        type:DEL_FAV_RECIPE,
+        id:id
+    }
+}
